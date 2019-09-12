dcd93adfd1ba0bbcd9a7b1401419e02616ceb24d
Taine Collins <tainecollins@Taines-MacBook-Pro.local>
2019-09-12 21:18:59 +1200

    local changes

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyCOniXaQmdSPdUELaAMQDjmkJZcEVOOLV4",
-    authDomain: "fitting-service.firebaseapp.com",
-    databaseURL: "https://fitting-service.firebaseio.com",
-    projectId: "fitting-service",
-    storageBucket: "fitting-service.appspot.com",
-    messagingSenderId: "125378256020"
+    apiKey: "AIzaSyBxMzSlKpIhFImQoD-gtnhL-DpxYqHk3oY",
+    authDomain: "hackathon-886b2.firebaseapp.com",
+    databaseURL: "https://hackathon-886b2.firebaseio.com",
+    projectId: "hackathon-886b2",
+    storageBucket: "",
+    messagingSenderId: "482382822066",
+    appId: "1:482382822066:web:50b1a47083fe818b5427ab"
   };
   firebase.initializeApp(config);
   export const db = firebase.firestore();
-  export const database =firebase.database();
+  export const database = firebase.database();
